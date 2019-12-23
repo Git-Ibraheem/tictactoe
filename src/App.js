f4e4b8a771f13ededcd698588e3c6cbadd83f2ca
afamily <afamily@afamilys-Mac-mini.local>
2019-12-22 17:54:49 -0800

    Bug testing

--- src/App.js
+++ src/App.js
@@ -2,35 +2,34 @@ import React from 'react';
 import './App.css';
 
 const Square = props => {
+  const [color, setColor] = React.useState("Black")
   let symbolCopy = [...props.symbol];
+  
   const handleClick = () => {
-    if (props.symbol[props.id] === "" ) {
-    if (props.turn%2 === 0){
-      symbolCopy[props.id] = "x";
-      props.setSymbol(symbolCopy)
-    } else {
-      symbolCopy[props.id] = "o";
-      props.setSymbol(symbolCopy)
+    if (props.symbol[props.id] === null) {
+      props.setTurn(props.turn + 1);
+      console.log("set turn")
+      if (props.turn % 2 === 0) {
+        symbolCopy[props.id] = "x";
+        props.setSymbol(symbolCopy)
+        console.log("set symbol - x")
+      } else {
+        symbolCopy[props.id] = "o";
+        props.setSymbol(symbolCopy)
+        console.log("set symbol - o")
+      }
     }
-  }
-    if (props.symbol[props.id] !== "" ) {
-      //console.log("dasdsadsadsaads")
-      //let turn = props.turn - 1;
-      //props.setTurn(turn);
-    }
+    declareWinner(props.symbol);
   };
-  return <button className="square" onClick={handleClick}>{props.symbol[props.id]}</button>
+
+  return <button className="square" style={{ color: color }} onClick={handleClick}>{props.symbol[props.id]}</button>
 };
 const SymbolDisplay = props => {
   const [turn, setTurn] = React.useState(0)
-  const [squares, setSquares] = React.useState(["","","","","","","","",""])
-  const handleClick = () => {
-    declareWinner(squares);
-    setTurn(turn + 1);
-  };
+  const [squares, setSquares] = React.useState([null, null, null, null, null, null, null, null, null,])
   return (
     <div className="game noSelect">
-      <div className="game-board" onClick={handleClick}>
+      <div className="game-board">
         <div className="board-row">
           <Square setTurn={setTurn} turn={turn} id={0} symbol={squares} setSymbol={setSquares} />
           <Square setTurn={setTurn} turn={turn} id={1} symbol={squares} setSymbol={setSquares} />
@@ -46,8 +45,12 @@ const SymbolDisplay = props => {
           <Square setTurn={setTurn} turn={turn} id={7} symbol={squares} setSymbol={setSquares} />
           <Square setTurn={setTurn} turn={turn} id={8} symbol={squares} setSymbol={setSquares} />
         </div>
-        <p>turn: {turn}</p>
-        <p>squares: {squares}</p>
+        <p>Turn: {turn}</p>
+        <tbody>
+          {squares.map(function (square, i) {
+            return <p>Square No. {i}: {squares[i]}</p>;
+          })}
+        </tbody>
       </div>
     </div>
   );
@@ -58,7 +61,7 @@ const Board = props => {
 };
 
 const utils = {
-  rows:[
+  rows: [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -73,12 +76,18 @@ const utils = {
 const declareWinner = (squares) => {
   utils.rows.forEach(
     x => {
-      if (squares[x[0]] !== "" && squares[x[1]] !== "" && squares[x[2]] !== "") {
-        if (squares[x[0]] === squares[x[1]] &&
-          squares[x[1]] === squares[x[2]]) {
+      console.log(x[0])
+      console.log(squares[x[0]])
+      console.log(x[1])
+      console.log(squares[x[1]])
+      console.log(x[2])
+      console.log(squares[x[2]])
+      if (squares[x[0]] !== null && squares[x[1]] !== null && squares[x[2]] !== null) {
+        if (squares[x[0]] === squares[x[1]] && squares[x[1]] === squares[x[2]]) {
           console.log("You Win!!!!!!")
         }
       }
+      console.log("--------------")
     }
   );
 };
